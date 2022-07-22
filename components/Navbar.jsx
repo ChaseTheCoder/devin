9ad6287dda83ff6012e9ac203c9524c2192f0879
Chase Sheaff <chasefsheaff@gmail.com>
2022-07-21 23:01:47 -0500

    feat: add mobile responsive hamburger navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const navItems = [{name: 'fitness'}, {name: 'eqx+'}, {name: 'modeling'}];
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <nav className="fixed top-0 left-0 w-full z-10 ease-in duration-300">
+      <div className="max-w-screen-xl m-auto flex justify-between items-center py-3 px-2">
+        <Link href="/">
+          <h1 className="font-bold text-xl sm:text-2xl uppercase">Devin Buchanan</h1>
+        </Link>
+        <ul className="hidden sm:flex">
+          {navItems.map((item) => (
+            <li className="py-4 pl-5"><Link key={item.name} href={`#${item.name}`}>{item.name}</Link></li>
+          ))}
+        </ul>
+        {/* Mobile Button */}
+        <div onClick={handleNav} className="block sm:hidden z-10 py-4">
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20}/>}
+        </div>
+        {/* Mobile Menu */}
+        <div className={
+          nav 
+            ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen text-center ease-in duration-300" 
+            : "sm:hidden absolute top-0 right-0 left-[-100%] bottom-0 flex justify-center items-center w-full h-screen text-center ease-in duration-300"
+            }
+          >
+          <ul>
+            {navItems.map((item) => (
+              <li className="p-4 text-4xl hover:text-yellow-300"><Link key={item.name} href={`#${item.name}`}>{item.name}</Link></li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import Head from 'next/head'
-import { Hero, Header } from '../components';
+import { Hero, Navbar } from '../components';
 import { getHeader } from '../services';
 
 export default function Home({ header }) {
   return (
-    <div className="container mx-auto px-10 max-w-screen-xl">
+    <div className="container mx-auto max-w-screen-xl">
       <Head>
         <title>Devin Buchanan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <Hero header={header} key={header.name}/>
+      <div className="p-5">
+        <Navbar/>
+        {/* <Hero header={header} key={header.name}/> */}
+      </div>
     </div>
   )
 }
