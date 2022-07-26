6c59a2a23aa213e4af1f235c33e91ab7495def58
Chase Sheaff <chasefsheaff@gmail.com>
2022-07-26 08:49:48 -0500

    style: hero section sizing

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({ header }) => {
         <h1 className="test-xl sm:text-4xl md:text-5xl lg:text-6xl font-bold uppercase">{header.name}</h1>
         <h2 className="text-sm md:text-2xl theme-color"><a href={header.socialHandleLink} target="_blank">{header.socialHandle}</a></h2>
       </div>
-        <img className="w-3/4 md:w-3/5 lg:w-1/2 absolute right-0 z-0" src={header.headerImage.url} alt={header.name} />
+        <img className="w-3/4 md:w-1/2 lg:w-3/5 absolute right-0 z-0" src={header.headerImage.url} alt={header.name} />
     </div>
   )
 }
