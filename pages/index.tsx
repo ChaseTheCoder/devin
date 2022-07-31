b43268d92701b088a073fe2a86d94300c7bc323b
Chase Sheaff <chasefsheaff@gmail.com>
2022-07-31 15:07:41 -0500

    feat: add html head meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,19 @@ export default function Home({ header, mission }: { header: any, mission: any}) 
   return (
     <div className="container mx-auto px-5">
       <Head>
-        <title>Devin Buchanan</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Devin Buchanan: Fitness, Modeling, Chicago Lifestyle</title>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="theme-color" content="#fffb00"/>
+        <meta name="robots" content="index,follow"/>
+        <meta name="description" content="Devin Buchanan's page for fitness, healthy diet, modeling, fashion, and Chicago lifestyle. Find his social links to Instagram and Tiktok, modeling portfolio, and more."/>
+        <meta name="keywords" content="fitness, diet, modeling, fashing, Chicago lifestyle"/>
+        <link rel="icon" href="/favicon.ico"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Devin Buchanan Fitness, healthy diet, modeling, fashing, Chicago lifestyle"/>
+        <meta property="og:description" content="Devin Buchanan's page for fitness, healthy diet, modeling, fashion, and Chicago lifestyle. Find his social links to Instagram and Tiktok, modeling portfolio, and more."/>
+        <meta property="og:url" content="https://www.devinbuchanan.com/"/>
+        <meta property="og:image" content="https://media.graphassets.com/HU6DpvnlQkiJ1tYrSs6E"/>
       </Head>
       <Navbar/>
       <Hero header={header} key={header.name}/>
