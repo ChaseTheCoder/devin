a45b037a471db60748847204a02249b1274db6cc
Chase Sheaff <chasefsheaff@gmail.com>
2023-03-15 18:31:20 -0500

    update: Head meta tags to make data cohesive and more focused

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,17 @@ export default function Home({ header, mission, modeling, equinox }: { header: a
   return (
     <div className="container mx-auto px-5">
       <Head>
-        <title>Devin Buchanan: Fitness, Modeling, Chicago Lifestyle</title>
+        <title>Devin Buchanan: Fitness & Modeling</title>
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <meta name="theme-color" content="#000000"/>
         <meta name="robots" content="index,follow"/>
-        <meta name="description" content="Devin Buchanan: Fitness & Modeling Portfolio."/>
+        <meta name="description" content="Devin Buchanan: Fitness & Modeling"/>
         <meta name="keywords" content="fitness, modeling, portfolio, Instagram, Tiktok, Chicago"/>
         <link rel="icon" href="/favicon.ico"/>
         <meta property="og:type" content="website"/>
         <meta property="og:title" content="Devin Buchanan: Fitness & Modeling"/>
-        <meta property="og:description" content="Devin Buchanan's fitness & modeling portfolio. Find his social links to Instagram and Tiktok."/>
+        <meta property="og:description" content="Find his social links to Instagram and Tiktok."/>
         <meta property="og:url" content="https://www.devinbuchanan.com/"/>
         <meta property="og:image" content="https://media.graphassets.com/HU6DpvnlQkiJ1tYrSs6E"/>
       </Head>
